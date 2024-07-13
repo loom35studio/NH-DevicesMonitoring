f3149719439d9330a5b7555a395cab496ce398f1
Davide <davide.gioia.private@gmail.com>
2024-07-13 04:32:51 +0200

    (final/working) fix UpdateStock with a mysql promises

--- routes/devices/update/updateStock.js
+++ routes/devices/update/updateStock.js
@@ -4,29 +4,31 @@ const convertDevicesToJSON = require("../generate/deviceList.js");
 
 
 async function updateStock(req) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const settings = await initializeSettings();
-      const { db } = settings;
-      let printer = req.body.printerName;
-      let action = req.body.tonerAction;
-      let toner = req.body.tonerSelection;
-      let note = req.body.note;
-      let type = "manuale";
-      // console.log(req.body);
+  const settings = await initializeSettings();
+  const { db } = settings;
+  try {
+    connection = await db.promise().getConnection();
 
-      if (action == "insert") {
-          db.query("UPDATE " + toner + " SET stock = stock + 1 WHERE printerID = " + printer);
-          updateLogs(printer, toner, note, type);
-      } else if (action == "remove") {
-          db.query("UPDATE " + toner + " SET stock = stock - 1 WHERE printerID = " + printer + " AND stock > 0")
-      }
-      resolve();
-    } catch (e) {
-      console.error("Errore durante l'aggiornamento dello stock:", e);
-      reject(e);
+    let printer = req.body.printerName;
+    let action = req.body.tonerAction;
+    let toner = req.body.tonerSelection;
+    let note = req.body.note;
+    let type = "manuale";
+    // console.log(req.body);
+
+    if (action == "insert") {
+        await connection.query("UPDATE " + toner + " SET stock = stock + 1 WHERE printerID = " + printer)
+        console.log("query inserimento riuscita");
+        updateLogs(printer, toner, note, type);
+    } else if (action == "remove") {
+        await connection.query("UPDATE " + toner + " SET stock = stock - 1 WHERE printerID = " + printer + " AND stock > 0")
+        console.log("query inserimento riuscita")
     }
-  });
+  } catch (e) {
+    console.error("Errore durante l'aggiornamento dello stock:", e);
+  }
 }
+
+
 
 module.exports = updateStock;
